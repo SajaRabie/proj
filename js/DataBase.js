88c898382d1e4fe9e97964890f64b3ae61769191
SajaRabie <2205250113std@gtc.edu.ps>
2022-08-21 16:11:17 +0300

    project

---
+++ js/DataBase.js
@@ -0,0 +1,301 @@
+var database = [{
+    "Jod_id": 1,
+    "Job_title": "Structural Engineer",
+    "Price": "$405.50",
+    "company": "Photobug",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 2,
+    "Job_title": "Paralegal",
+    "Price": "$475.53",
+    "company": "Eadel",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 3,
+    "Job_title": "Associate Professor",
+    "Price": "$729.73",
+    "company": "Divanoodle",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 4,
+    "Job_title": "Information Systems Manager",
+    "Price": "$300.70",
+    "company": "Feedmix",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 5,
+    "Job_title": "Technical Writer",
+    "Price": "$854.12",
+    "company": "Agimba",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 6,
+    "Job_title": "Senior Financial Analyst",
+    "Price": "$457.11",
+    "company": "Kamba",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 7,
+    "Job_title": "Product Engineer",
+    "Price": "$203.92",
+    "company": "Dazzlesphere",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 8,
+    "Job_title": "VP Accounting",
+    "Price": "$943.25",
+    "company": "Browsedrive",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 9,
+    "Job_title": "Operator",
+    "Price": "$297.52",
+    "company": "Vitz",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 10,
+    "Job_title": "Human Resources Manager",
+    "Price": "$408.94",
+    "company": "Meedoo",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 11,
+    "Job_title": "Office Assistant II",
+    "Price": "$946.88",
+    "company": "Agivu",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 12,
+    "Job_title": "Associate Professor",
+    "Price": "$833.16",
+    "company": "Vinte",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 13,
+    "Job_title": "Social Worker",
+    "Price": "$452.52",
+    "company": "Brainbox",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 14,
+    "Job_title": "Executive Secretary",
+    "Price": "$590.81",
+    "company": "Skiba",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 15,
+    "Job_title": "Senior Developer",
+    "Price": "$948.75",
+    "company": "Zoomdog",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 16,
+    "Job_title": "Paralegal",
+    "Price": "$434.23",
+    "company": "Babblestorm",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 17,
+    "Job_title": "Structural Analysis Engineer",
+    "Price": "$171.96",
+    "company": "Topiczoom",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 18,
+    "Job_title": "Civil Engineer",
+    "Price": "$562.39",
+    "company": "Gabtune",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 19,
+    "Job_title": "Compensation Analyst",
+    "Price": "$198.27",
+    "company": "Brainverse",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 20,
+    "Job_title": "Junior Executive",
+    "Price": "$924.28",
+    "company": "Devcast",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 21,
+    "Job_title": "Marketing Manager",
+    "Price": "$433.28",
+    "company": "Kwideo",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 22,
+    "Job_title": "Computer Systems Analyst II",
+    "Price": "$736.32",
+    "company": "Flashspan",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 23,
+    "Job_title": "General Manager",
+    "Price": "$512.98",
+    "company": "Youtags",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 24,
+    "Job_title": "Executive Secretary",
+    "Price": "$585.22",
+    "company": "Wordware",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 25,
+    "Job_title": "Mechanical Systems Engineer",
+    "Price": "$827.03",
+    "company": "Fatz",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 26,
+    "Job_title": "Pharmacist",
+    "Price": "$479.66",
+    "company": "Thoughtworks",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 27,
+    "Job_title": "Web Designer III",
+    "Price": "$576.19",
+    "company": "Skinix",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 28,
+    "Job_title": "Marketing Manager",
+    "Price": "$244.95",
+    "company": "Yakidoo",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 29,
+    "Job_title": "Account Coordinator",
+    "Price": "$901.20",
+    "company": "Eabox",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 30,
+    "Job_title": "Speech Pathologist",
+    "Price": "$254.44",
+    "company": "Tazz",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 31,
+    "Job_title": "Web Developer IV",
+    "Price": "$136.40",
+    "company": "Jabbersphere",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 32,
+    "Job_title": "Research Assistant II",
+    "Price": "$924.79",
+    "company": "Rhyzio",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 33,
+    "Job_title": "Research Nurse",
+    "Price": "$140.84",
+    "company": "Quatz",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 34,
+    "Job_title": "Operator",
+    "Price": "$619.33",
+    "company": "Edgeify",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 35,
+    "Job_title": "Environmental Specialist",
+    "Price": "$527.15",
+    "company": "Tagchat",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 36,
+    "Job_title": "Chief Design Engineer",
+    "Price": "$769.98",
+    "company": "Realcube",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 37,
+    "Job_title": "Assistant Manager",
+    "Price": "$608.24",
+    "company": "Innotype",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 38,
+    "Job_title": "Human Resources Assistant I",
+    "Price": "$158.38",
+    "company": "Quinu",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 39,
+    "Job_title": "Sales Representative",
+    "Price": "$172.06",
+    "company": "Flipstorm",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 40,
+    "Job_title": "Data Coordiator",
+    "Price": "$382.57",
+    "company": "Riffpath",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 41,
+    "Job_title": "Marketing Manager",
+    "Price": "$851.22",
+    "company": "Skinte",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 42,
+    "Job_title": "Social Worker",
+    "Price": "$146.67",
+    "company": "Chatterpoint",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 43,
+    "Job_title": "VP Accounting",
+    "Price": "$866.73",
+    "company": "Quatz",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 44,
+    "Job_title": "Budget/Accounting Analyst III",
+    "Price": "$461.04",
+    "company": "Yacero",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 45,
+    "Job_title": "Software Test Engineer III",
+    "Price": "$157.29",
+    "company": "Quimba",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 46,
+    "Job_title": "Research Nurse",
+    "Price": "$526.52",
+    "company": "Brightbean",
+    "contract_by": "fixed"
+}, {
+    "Jod_id": 47,
+    "Job_title": "Quality Engineer",
+    "Price": "$681.09",
+    "company": "Zoozzy",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 48,
+    "Job_title": "Pharmacist",
+    "Price": "$420.63",
+    "company": "Gevee",
+    "contract_by": "daily"
+}, {
+    "Jod_id": 49,
+    "Job_title": "Recruiting Manager",
+    "Price": "$102.30",
+    "company": "Mynte",
+    "contract_by": "hourly"
+}, {
+    "Jod_id": 50,
+    "Job_title": "Chemical Engineer",
+    "Price": "$654.45",
+    "company": "Dynava",
+    "contract_by": "daily"
+}];
